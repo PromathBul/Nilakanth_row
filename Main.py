127e731e7fa436c18bbff55d485609f99cac4cb9
Dmitry_Tikhonov <tdmv1209@gmail.com>
2022-10-20 18:41:02 +0300

    Finished calling functions in file 'Main.py'

--- Main.py
+++ Main.py
@@ -1,7 +1,8 @@
 # 3,1415926535897932384626433832795
 
+from Methods import enter
+from Methods import calculate_pi as calc
 
-
-accuracy = int(input('Введите количество значащих цифр после запятой, которые нужно определить в числе пи: '))
-pi = calculate_pi(accuracy)
+accuracy = enter('Введите количество значащих цифр после запятой, которые нужно определить в числе пи: ')
+pi = calc(accuracy)
 print(f'Число пи с точностью до {accuracy}-го знака равно {pi}')
